fc1aad10db2ce1486818abc51c2ed7395aec8245
Rawanbahaa142 <rwanalrawy162@gmail.com>
2026-04-18 19:32:27 +0200

    forPull

--- src/app/features/Auth/auth-layout/auth-layout.ts
+++ src/app/features/Auth/auth-layout/auth-layout.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from "@angular/router";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './auth-layout.html',
   styleUrl: './auth-layout.css',
 })
 export class AuthLayout {
 constructor(private router: Router) { }
-  back() {
-    this.router.navigate(['/landing']);
-  }
+
 }
