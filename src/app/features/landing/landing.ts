e39f400b51a9e0190c74c8a2705edb63a5ff5ba7
Reham <rehamahmedfek@gmail.com>
2026-04-19 14:07:13 +0200

    home page

--- src/app/features/landing/landing.ts
+++ src/app/features/landing/landing.ts
@@ -6,11 +6,13 @@ import { HowItWorksComponent } from "./how-it-works/how-it-works.component";
 import { WorkersSectionComponent } from "./workers-section/workers-section.component";
 import { ReviewsSectionComponent } from "./reviews-section/reviews-section.component";
 import { ContactUsComponent } from "./Contact-us/Contact-us.component";
+import { RouterOutlet } from "@angular/router";
+
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [NavBar, HeroSection, ServicesSectionComponent, HowItWorksComponent, WorkersSectionComponent, ReviewsSectionComponent, ContactUsComponent],
+  imports: [NavBar, HeroSection, ServicesSectionComponent, HowItWorksComponent, WorkersSectionComponent, ReviewsSectionComponent, ContactUsComponent, RouterOutlet],
   templateUrl: './landing.html',
   styleUrl: './landing.css'
 })
